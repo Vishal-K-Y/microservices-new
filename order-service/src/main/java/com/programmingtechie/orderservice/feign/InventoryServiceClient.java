6a95eb43aab71bad319a6b353083d8bb3b55b831
vishalk <vishalyadav10558@gmail.com>
2025-04-20 16:48:40 +0530

    adding feignClient
    calling inventory from order-service

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/feign/InventoryServiceClient.java
@@ -0,0 +1,16 @@
+package com.programmingtechie.orderservice.feign;
+
+import com.programmingtechie.orderservice.dto.InventoryResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name="inventory-service", url="http://localhost:7779")
+public interface InventoryServiceClient {
+
+    @GetMapping("/api/inventory")
+    List<InventoryResponse> isInStock(@RequestParam List<String> skuCode);
+}
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.programmingtechie.orderservice.service;
 
-import com.programmingtechie.orderservice.client.InventoryClient;
+import com.programmingtechie.orderservice.dto.InventoryResponse;
+import com.programmingtechie.orderservice.feign.InventoryServiceClient;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.feign.InventoryServiceClient;
 import com.programmingtechie.orderservice.model.Order;
 import com.programmingtechie.orderservice.model.OrderLineItems;
 import com.programmingtechie.orderservice.repository.OrderRepository;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,6 +23,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryServiceClient inventoryServiceClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -32,7 +36,24 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        System.out.println(skuCodes);
+
+        List<InventoryResponse> inventoryResponsList= inventoryServiceClient.isInStock(skuCodes);
+
+        System.out.println(inventoryResponsList);
+        //orderRepository.save(order);
+        boolean allProductsInStock = skuCodes.size()==inventoryResponsList.size();
+
+        System.out.println("allProductsInStock-> "+allProductsInStock);
+        if(allProductsInStock){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDtO(OrderLineItemsDto orderLineItemsDto){
